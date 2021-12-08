acb5d5a0c14eebfee09ff9c8a22a3c1efe5875c2
JbirdL86 <jgonzalez.wj@gmail.com>
2021-12-08 08:56:30 -0600

    Fix linterns errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
   end
   root to: 'users#index'
 end
