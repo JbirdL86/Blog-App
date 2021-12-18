377341dfce63e7ed5222ca4e65a6c8361414ac91
JbirdL86 <jgonzalez.wj@gmail.com>
2021-12-18 08:55:34 -0600

    Add routes for new post, comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index new create show]
   end
+
+  resources :posts do
+    resources :likes, only: %i[create]
+    resources :comments, only: %i[new create]
+  end
+
   root to: 'users#index'
 end
