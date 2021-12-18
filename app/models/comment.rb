d6f89c27d593f05b48ce17acd3a7fa36e5e5339c
JbirdL86 <jgonzalez.wj@gmail.com>
2021-12-18 16:39:31 -0600

    Change as required

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   belongs_to :post, foreign_key: :post_id
+  has_many :likes, foreign_key: :post_id
   after_save :update_comments_count
+  default_scope { includes(:likes) }
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   validates :name, presence: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  default_scope { includes(:posts) }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
