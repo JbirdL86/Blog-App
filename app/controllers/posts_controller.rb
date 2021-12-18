7167d26af3923e67655746eb2be00f6e1a087c51
JbirdL86 <jgonzalez.wj@gmail.com>
2021-12-18 08:51:52 -0600

    Create necessary variables for create and new actions for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,24 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def create
+    @post = Post.create(post_params)
+    @user = current_user
+    @post.author_id = @user.id
+    @post.comments_counter = 0
+    if @post.save
+      redirect_to user_path(@post.author_id)
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
+  end
 end
