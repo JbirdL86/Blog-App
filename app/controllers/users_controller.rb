9694f6944d098a858eef12c03fd590c40096b5d9
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-22 04:56:14 +0500

    Add styling and update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @current_user = current_user
   end
-
   def show
     @user = User.find(params[:id])
     @current_user = current_user
     @posts = @user.recent_posts
+  end
+  def create
+    @user = User.create(user_params)
+  end
+  def edit
+    current_user.update(user_params)
+  end
+  private
+  def users_params
+    params.require(:user).permit(:name, :bio, :photo, :posts_counter, :email, :password)
   end
 end
