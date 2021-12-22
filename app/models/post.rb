18c929acb7fc1b5105ba8cba4117ad0a12d463bd
JbirdL86 <jgonzalez.wj@gmail.com>
2021-12-21 18:27:27 -0600

    Remove includes since there none n +1 problems

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,6 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
   validates :text, presence: true
   validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  default_scope { includes(:comments) }
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
